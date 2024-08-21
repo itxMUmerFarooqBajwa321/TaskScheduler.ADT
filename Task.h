21640143d203be16ce4287046861b503734d357d
Muhammad Umer Farooq Bajwa <umerfarooqbajwa321@gmail.com>
2024-08-21 14:27:33 +0500

    Update Task.h
    Remove Definitions of class from here and copy them to .cpp file

--- Task.h
+++ Task.h
@@ -27,48 +27,4 @@ public:
 	to handle, not responsibility of Task class.
 	*/
 };
-Task::Task() : taskDate{}, taskTime{}, taskMsg{}
-{}
-Task::Task(const Date& d, const Time& t, const String& m):  taskDate{d}, taskTime{t}, taskMsg{m}
-{
-	/*taskDate = d;
-	taskTime = t;
-	taskMsg.reSize(m.getSize());
-	taskMsg.concatEqual(m);*/
-}
-void Task::updateDate(const Date& nd)
-{
-	taskDate.setDate(nd);
-}
-void Task::updateTime(const Time& nt)
-{
-	taskTime.setTime(nt);
-}
-void Task::updateMessage(const String& nm)
-{
-	taskMsg.~String();
-	taskMsg.reSize(nm.getLength() + 1);
-	//taskMsg.at(0) = '\0';
-	taskMsg+=nm;
-}
-Date Task::getDate() const
-{
-	return taskDate;
-}
-Time Task::getTime() const
-{
-	return taskTime;
-}
-String Task::getMessage() const
-{
-	return taskMsg;
-}
-void Task::print() const
-{
-	taskMsg.display();
-	cout << " at ";
-	taskTime.printTwentyFourHourFormat();
-	cout << " of ";
-	taskDate.printFormat1();
-}
 #endif
